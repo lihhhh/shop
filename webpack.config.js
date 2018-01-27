e5f680690fbc0dd1cfbf07c9aff39361f1d3ce07
lihhhh <632598678@qq.com>
2018-01-27 23:43:26 +0800

    bug

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,7 @@ module.exports = function(cfg){
                 use: {
                     loader:'file-loader',
                     options: {
+                        // name:'Images/Seller/Template/WeChat/[hash].[ext]'
                         name:'/Images/Seller/Template/WeChat/[hash].[ext]'
                     }
                 }
